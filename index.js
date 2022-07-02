fb84cbfbad4dd5c3aea238d996f9fe1c19d17588
Wesley Wei <wesleytianqiwei@gmail.com>
2022-07-02 09:48:39 -0400

    n-lever array sum

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+const sumItems = function(array) {
+  for (let i in array) {
+    if (Array.isArray(array[i])) {
+      let num = sumItems(array[i]);
+      //console.log(num);
+      array[i] = num;
+      //console.log(array);
+    }
+  }
+  return array.reduce((prev, cur) =>prev + cur, 0);
+    
+   
+};
+const array = [[1, 2, [[3], 4]], 5, []];
 
+console.log(sumItems(array));
 module.exports = sumItems;
